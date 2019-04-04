d3306f93a2c36fb1b7c4e205d8e532da23051e85
Nicholas Knight <38379829+Nickkick909@users.noreply.github.com>
2019-04-03 20:28:40 -0400

    worked with item service

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -10,14 +10,14 @@ import { Storage } from '@ionic/storage';
 })
 export class ItemService {
   //define some data containers such as arrays to store items
-  items:Observable<any[]>;
-  orders:Observable<any[]>;
+  games:Observable<any[]>;
+
   user:Array<any>=[{"uid":"testid", "type":"testtype"}];
   refu=firebase.database().ref('usertypes/');
   constructor(public db: AngularFirestore, private storage: Storage, public events:Events) { 
-    console.log("loading saved items and orders");
-    this.items = db.collection('items').valueChanges();
-    this.orders = db.collection('orders').valueChanges();
+    console.log("loading saved games and orders");
+    this.games = db.collection('games').valueChanges();
+    
     this.refu.on('value',resp =>{
       this.user=[];
       this.user=snapshotToArray(resp);
@@ -27,66 +27,22 @@ export class ItemService {
 
   //provide functions to get items
   getItems(){
-    console.log('getting items...' + this.items);
-    return this.items;
+    console.log('getting items...' + this.games);
+    return this.games;
   }
 
-  //get them orders boi
-  getOrders(){
-    console.log('gettings orders...' + this.orders);
-    return this.orders;
-  }
 
-  createItem(name, price, category, photoUrl,description){
+  createGame(name, icon){
     let randomId = Math.random().toString(36).substr(2,5);
     this.db.collection('/items').add({
       "id":randomId, 
       "name":name, 
-      "price": price, 
-      "category":category, 
-      "photoUrl":photoUrl, 
-      "description":description
+      "icon" : icon
     });
-/*
-    console.log("now saving all items:");
-    let usersStringifiedObj = JSON.stringify(this.items);
-    localStorage.setItem("items", usersStringifiedObj);*/
 
-    // let loadeditems= JSON.parse(localStorage.getItem("items"));
-    // console.log("saved no. of items"+loadeditems.length.toString())
   }
 
-  editSettings(storeName, logo, phone, address){
-    this.db.collection('/settings').add({
-      "storeName":storeName, 
-      "logo": logo, 
-      "phone":phone, 
-      "address":address
-    });
-  }
 
-  /*updateItem(newValues){
-    let itemIndex = this.items.findIndex(item => item.id == newValues.id);
-    
-    if(newValues.photoUrl == undefined){
-      newValues.photoUrl = this.items[itemIndex].photoUrl
-    }
-
-    this.items[itemIndex] = newValues;
-    console.log(newValues.photoUrl);
-  }*/
-
-  createOrder(quantity, total_price, item_name,date) {
-    let randomId = Math.random().toString(36).substr(2,5);
-    let uid= firebase.auth().currentUser.uid;
-    this.db.collection('/orders').add({
-      "id":uid, 
-      "quantity":quantity, 
-      "total_price": total_price, 
-      "item_name":item_name, 
-      "date":date
-    });
-  }
   getUser(){
     for(var i=0;i<this.user.length;i++) {
       if(this.user[i].uid==firebase.auth().currentUser.uid) {
