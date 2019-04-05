84e1517e1ea201c80a19875e5b1643ffd9576d84
edward-gp <boredstrattus@gmail.com>
2019-04-05 09:21:29 -0400

    added code to start upload of images. Also installed cordova/ionic stuff for file import

--- src/app/new-game/new-game.page.ts
+++ src/app/new-game/new-game.page.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
+import { File } from "@ionic-native/file/ngx";
 
+import * as firebase from "firebase";
 @Component({
   selector: 'app-new-game',
   templateUrl: './new-game.page.html',
   styleUrls: ['./new-game.page.scss'],
 })
 export class NewGamePage implements OnInit {
-
-  constructor() { }
+  result;
+  imgfile="";
+  constructor(private file: File) { }
 
   ngOnInit() {
   }
 
+
+
+// FILE STUFF
+makeFileIntoBlob(_imagePath) {
+  // INSTALL PLUGIN - cordova plugin add cordova-plugin-file
+  return new Promise((resolve, reject) => {
+    let fileName = "";
+    this.file
+      .resolveLocalFilesystemUrl(_imagePath)
+      .then(fileEntry => {
+        let { name, nativeURL } = fileEntry;
+
+        // get the path..
+        let path = nativeURL.substring(0, nativeURL.lastIndexOf("/"));
+        console.log("path", path);
+        console.log("fileName", name);
+
+        fileName = name;
+
+        // we are provided the name, so now read the file into
+        // a buffer
+        return this.file.readAsArrayBuffer(path, name);
+      })
+      .then(buffer => {
+        // get the buffer and make a blob to be saved
+        let imgBlob = new Blob([buffer], {
+          type: "image/jpeg"
+        });
+        console.log(imgBlob.type, imgBlob.size);
+        resolve({
+          fileName,
+          imgBlob
+        });
+      })
+      .catch(e => reject(e));
+  });
+}
+
+   /**
+   *
+   * @param _imageBlobInfo
+   */
+  uploadToFirebase(_imageBlobInfo) {
+    console.log("uploadToFirebase");
+    return new Promise((resolve, reject) => {
+      let imageid = (Math.floor(Math.random() * 2000)).toString();
+      let filename = "menu_"+imageid
+      // filename = _imageBlobInfo.fileName;
+      let fileRef = firebase.storage().ref("images/" + filename);
+
+      let uploadTask = fileRef.put(_imageBlobInfo.imgBlob);
+      let mydownloadurl="";
+      
+
+      uploadTask.on(
+        "state_changed",
+        (_snapshot: any) => {
+          console.log(
+            "snapshot progess " +
+              (_snapshot.bytesTransferred / _snapshot.totalBytes) * 100
+          );
+        },
+        _error => {
+          console.log(_error);
+          reject(_error);
+        },
+        () => {
+          // completion...  get the image URL for saving to database
+          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            console.log('File available at', downloadURL);
+            mydownloadurl = downloadURL;
+            resolve( mydownloadurl);
+          });
+          // resolve( uploadTask.snapshot);
+          // resolve( mydownloadurl);
+
+        }
+      );
+    });
+  }
 }
