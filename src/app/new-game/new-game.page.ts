8fa3871a5be1ff962042fd8b14a075209f32a264
Nicholas Knight <38379829+Nickkick909@users.noreply.github.com>
2019-04-10 22:48:14 -0400

    working on form for new game (img not working)

--- src/app/new-game/new-game.page.ts
+++ src/app/new-game/new-game.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { File } from "@ionic-native/file/ngx";
 import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 // import { PhotoLibrary } from '@ionic-native/photo-library/ngx';
-// import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ItemService } from '../item.service';
@@ -15,43 +15,24 @@ import * as firebase from "firebase";
 })
 export class NewGamePage implements OnInit {
   result;
-  imgfile="";
+  imgfile= "";
+
+  new_game: FormGroup;
   constructor(private file: File, private camera: Camera,
     private router: Router,
- 	  //public formBuilder: FormBuilder,
+ 	  public formBuilder: FormBuilder,
  	     public itemService: ItemService) { }
   //constructor parameters break add page  private photoLibrary: PhotoLibrary
-  ngOnInit() :void {}
-
-
-
-/*pickImage() {
-  this.photoLibrary.requestAuthorization().then(() => {
-    this.photoLibrary.getLibrary().subscribe({
-      next: library => {
-        library.forEach(function(libraryItem) {
-          console.log(libraryItem.id);          // ID of the photo
-          console.log(libraryItem.photoURL);    // Cross-platform access to photo
-          console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
-          console.log(libraryItem.fileName);
-          console.log(libraryItem.width);
-          console.log(libraryItem.height);
-          console.log(libraryItem.creationDate);
-          console.log(libraryItem.latitude);
-          console.log(libraryItem.longitude);
-          console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
-        });
-      },
-      error: err => { console.log('could not get photos'); },
-      complete: () => { console.log('done getting photos'); }
+  ngOnInit() :void {
+    this.new_game = this.formBuilder.group({
+      title: new FormControl('', Validators.required),
+     
     });
-  })
-  .catch(err => console.log('permissions weren\'t granted'));
-}*/
+  }
 
   createGame(value){
     //this.itemService.createGame(value.title,value.img);
-    this.itemService.createGame("testGameTitle", "testImage");
+    this.itemService.createGame(value.title, this.imgfile);
   	this.goBack();
   }
   goBack() {
